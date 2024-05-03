7dc2469f7fca704034afa4171a31f037c9cb8cdf
Ashley Kapaso <ashleykapaso@gmail.com>
2024-05-04 00:42:43 +0200

    add simple functionality

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 import os
 
 from dotenv import load_dotenv
+from discord import Intents, Client, Message
 
 load_dotenv()
 
-print(os.getenv("DISCORD_TOKEN"))
+intents = Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+
+async def send_message(message, user_message):
+    response = f'Hello {message.author}, you said: "{user_message}"'
+    await message.channel.send(response)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} is running')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    username = str(message.author)
+    user_message = message.content
+    channel = str(message.channel)
+
+    print(f'{username} said: {user_message} in {channel}')
+
+    await send_message(message, user_message)
+
+def main():
+    client.run(token=os.getenv('DISCORD_TOKEN'))
+
+if __name__ == '__main__':
+    main()
